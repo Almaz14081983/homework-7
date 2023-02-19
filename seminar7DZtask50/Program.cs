dfb39ecc1b1235dec2d6e70a27b4daa08c04349a
Almaz <minnexanov.a@mail.ru>
2023-02-19 11:23:11 +0300

    seminar 7 dz Task 50

--- seminar7DZtask50/Program.cs
+++ seminar7DZtask50/Program.cs
@@ -43,7 +43,6 @@ int[,] GetArray(int m, int n, int min, int max)
 void ElementPosition (int[,] matrix)
 {
     int count = 0;
-    int[,] result = new int [matrix.GetLength(0),matrix.GetLength(1)];
     for(int i = 0; i < matrix.GetLength(0); i ++)
     {
         for(int j = 0; j < matrix.GetLength(1); j ++)
